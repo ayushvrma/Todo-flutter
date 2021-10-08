b8754f705acc36a4bc2c666cd021e93d7cca9f65
ayushvrma <ayush8402@gmail.com>
2021-10-09 02:28:04 +0530

    idk why not generating file

--- lib/database/moor_database.dart
+++ lib/database/moor_database.dart
@@ -1,4 +1,6 @@
-import 'package:moor/moor.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:moor_flutter/moor_flutter.dart';
+
 part 'moor_database.g.dart'; //generated file
 
 @DataClassName('Tasks Table')
@@ -8,4 +10,15 @@ class Tasks extends Table {
   TextColumn get name => text().withLength(min: 1, max: 50)();
   DateTimeColumn get dueData => dateTime().nullable()();
   BoolColumn get isDome => boolean().withDefault(Constant(false))();
+}
+
+@UseMoor(tables: [Tasks])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase()
+      : super(FlutterQueryExecutor.inDatabaseFolder(
+            path: 'db.sqlite',
+            logStatements:
+                true)); //calling the super constructor, db.sqlite name doesnt matter
+  @override
+  int get schemaVersion => 1;
 }
